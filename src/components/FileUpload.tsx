662d463795234805b87b1a831ae8e693c9bb9a3b
Harshit Aditya <harshitaditya27@gmail.com>
2023-09-24 01:02:07 +0530

    Added AWS bucket component for file storage

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { uploadtoS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import { useDropzone } from "react-dropzone";
 
@@ -7,8 +8,19 @@ const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("Please upload a smaller file");
+        return;
+      }
+      try {
+        const data = await uploadtoS3(file);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
