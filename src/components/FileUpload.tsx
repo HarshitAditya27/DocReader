08cd9195463a79a8690d184bb377d02501faff49
Harshit Aditya <harshitaditya27@gmail.com>
2023-09-24 12:00:12 +0530

    Added react query setup

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,10 +1,31 @@
 "use client";
 
 import { uploadtoS3 } from "@/lib/s3";
-import { Inbox } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader2 } from "lucide-react";
+import React from "react";
 import { useDropzone } from "react-dropzone";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const FileUpload = () => {
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chart", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
@@ -12,14 +33,28 @@ const FileUpload = () => {
       console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        alert("Please upload a smaller file");
+        toast.error("Please upload a smaller file");
         return;
       }
       try {
+        setUploading(true);
         const data = await uploadtoS3(file);
-        console.log(data);
+        if (!data?.file_key || !data.file_name) {
+          alert("Error");
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            toast.success(data.message);
+          },
+          onError: (err) => {
+            toast.error("Error creating chart");
+          },
+        });
       } catch (error) {
         console.log(error);
+      } finally {
+        setUploading(false);
       }
     },
   });
@@ -33,10 +68,17 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
-        <>
-          <Inbox className="w-10 h-10 text-blue-500" />
-          <p className="mt-2 text-sm text-slate-600  ">Drop PDF here</p>
-        </>
+        {uploading || isLoading ? (
+          <>
+            <Loader2 className="h-10 w-10 text-green-400 animate-spin" />
+            <p className="mt-2 text-sm text-slate-200">GPT is thinking 🤔...</p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-600  ">Drop PDF here</p>
+          </>
+        )}
       </div>
     </div>
   );
